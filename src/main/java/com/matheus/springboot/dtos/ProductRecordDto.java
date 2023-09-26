52684dd127881848fca1a275a850fbb628a7b0cf
matheus <matheusvalencasilva2022@gmail.com>
2023-09-26 20:24:54 -0300

    complete commit

---
+++ src/main/java/com/matheus/springboot/dtos/ProductRecordDto.java
@@ -0,0 +1,9 @@
+package com.matheus.springboot.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+}
